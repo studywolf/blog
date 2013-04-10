9b4183b1a15f81fa138f37135da37b5538bf5fcb
studywolf <travis.dewolf@gmail.com>
2013-04-10 13:53:58 -0700

    Added inverse kinematics example for 3link arm
    - uses scipy.optimize.slsqp for constrained minimization
    - weights joints differently for staying close to resting state
    - inequality constraint ensures that joints stay within right arm limits
    - also have graphical arm plotter that moves arm to track mouse position

---
+++ InvKin/Arm.py
@@ -0,0 +1,162 @@
+# Written by Travis DeWolf (April, 2013)
+
+import math
+import numpy as np
+import scipy.optimize  
+
+class Arm3Link:
+    
+    def __init__(self, q=None, q0=None, L=None):
+        """Set up the basic parameters of the arm.
+        All lists are in order [shoulder, elbow, wrist].
+        
+        :param list q: the initial joint angles of the arm
+        :param list q0: the default (resting state) joint configuration
+        :param list L: the arm segment lengths
+        """
+        # initial joint angles
+        if q is None: q = [.3, .3, 0]
+        self.q = q
+        # some default arm positions
+        if q0 is None: q0 = np.array([math.pi/4, math.pi/4, math.pi/4]) 
+        self.q0 = q0
+        # arm segment lengths
+        if L is None: L = np.array([1, 1, 1]) 
+        self.L = L
+        
+        self.max_angles = [math.pi, math.pi, math.pi/4]
+        self.min_angles = [0, 0, -math.pi/4]
+
+    def get_xy(self, q=None):
+        """Returns the corresponding hand xy coordinates for 
+        a given set of joint angle values [shoulder, elbow, wrist], 
+        and the above defined arm segment lengths, L
+        
+        :param list q: the list of current joint angles
+        :returns list: the [x,y] position of the arm
+        """
+        if q is None: q = self.q
+
+        x = self.L[0]*np.cos(q[0]) + \
+            self.L[1]*np.cos(q[0]+q[1]) + \
+            self.L[2]*np.cos(np.sum(q))
+
+        y = self.L[0]*np.sin(q[0]) + \
+            self.L[1]*np.sin(q[0]+q[1]) + \
+            self.L[2]*np.sin(np.sum(q))
+
+        return [x, y]
+
+    def inv_kin(self, xy):
+        """This is just a quick write up to find the inverse kinematics
+        for a 3-link arm, using the SciPy optimize package minimization function.
+
+        Given an (x,y) position of the hand, return a set of joint angles (q)
+        using constraint based minimization, constraint is to match hand (x,y), 
+        minimize the distance of each joint from it's default position (q0).
+        
+        :param list xy: a tuple of the desired xy position of the arm
+        :param list q: initial set of joint angles [shoulder, elbow, wrist]
+        :param list q0: default set of joint angles [shoulder, elbow, wrist]
+        :param list L: set of arm segment lengths [shoulder, elbow, wrist]
+        :returns list: the optimal [shoulder, elbow, wrist] angle configuration
+        """
+
+        def distance_to_default(q, *args): 
+            """Objective function to minimize
+            Calculates the euclidean distance through joint space to the default
+            arm configuration. The weight list allows the penalty of each joint 
+            being away from the resting position to be scaled differently, such
+            that the arm tries to stay closer to resting state more for higher 
+            weighted joints than those with a lower weight.
+            
+            :param list q: the list of current joint angles
+            :returns scalar: euclidean distance to the default arm position
+            """
+            # weights found with trial and error, get some wrist bend, but not much
+            weight = [1, 1, 1.3] 
+            return np.sqrt(np.sum([(qi - q0i)**2 * wi
+                for qi,q0i,wi in zip(q, self.q0, weight)]))
+
+        def x_constraint(q, xy):
+            """Returns the corresponding hand xy coordinates for 
+            a given set of joint angle values [shoulder, elbow, wrist], 
+            and the above defined arm segment lengths, L
+            
+            :param list q: the list of current joint angles
+            :returns: the difference between current and desired x position
+            """
+            x = ( self.L[0]*np.cos(q[0]) + self.L[1]*np.cos(q[0]+q[1]) + 
+                self.L[2]*np.cos(np.sum(q)) ) - xy[0]
+            return x
+
+        def y_constraint(q, xy): 
+            """Returns the corresponding hand xy coordinates for 
+            a given set of joint angle values [shoulder, elbow, wrist], 
+            and the above defined arm segment lengths, L
+            
+            :param list q: the list of current joint angles
+            :returns: the difference between current and desired y position
+            """
+            y = ( self.L[0]*np.sin(q[0]) + self.L[1]*np.sin(q[0]+q[1]) + 
+                self.L[2]*np.sin(np.sum(q)) ) - xy[1]
+            return y
+
+        def joint_angle_constraints(q, xy):
+            """This is an inequality constraint, where we force the 
+            arm to be the right arm, by putting an upper and lower 
+            bound on possible joint angles.
+
+            :returns list: where each element must be >= 0 to be valid
+            """
+            return [q - self.min_angles, self.max_angles - q]
+        
+        return scipy.optimize.fmin_slsqp( func=distance_to_default, 
+            x0=self.q, eqcons=[x_constraint, y_constraint], 
+            f_ieqcons=joint_angle_constraints,
+            args=[xy], iprint=0) # iprint=0 suppresses output
+
+def test():
+    ############Test it!##################
+
+    arm = Arm3Link()
+
+    # set of desired (x,y) hand positions
+    x = np.arange(-.75, .75, .05)
+    y = np.arange(0, .75, .05)
+
+    # threshold for printing out information, to find trouble spots
+    thresh = .025
+
+    count = 0
+    total_error = 0
+    # test it across the range of specified x and y values
+    for xi in range(len(x)):
+        for yi in range(len(y)):
+            # test the inv_kin function on a range of different targets
+            xy = [x[xi], y[yi]]
+            # run the inv_kin function, get the optimal joint angles
+            q = arm.inv_kin(xy=xy)
+            # find the (x,y) position of the hand given these angles
+            actual_xy = arm.get_xy(q)
+            # calculate the root squared error
+            error = np.sqrt((np.array(xy) - np.array(actual_xy))**2)
+            # total the error 
+            total_error += error
+            
+            # if the error was high, print out more information
+            if np.sum(error) > thresh:
+                print '-------------------------'
+                print 'Initial joint angles', arm.q 
+                print 'Final joint angles: ', q
+                print 'Desired hand position: ', xy
+                print 'Actual hand position: ', actual_xy
+                print 'Error: ', error
+                print '-------------------------'
+            
+            count += 1
+
+    print '\n---------Results---------'
+    print 'Total number of trials: ', count
+    print 'Total error: ', total_error
+    print '-------------------------'
