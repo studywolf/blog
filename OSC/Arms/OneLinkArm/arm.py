709fba921fb44f90f638a5993e2e2fc5846abb66
travis dewolf <travis.dewolf@gmail.com>
2013-12-10 10:40:14 -0500

    Update

--- OSC/Arms/OneLinkArm/arm.py
+++ OSC/Arms/OneLinkArm/arm.py
@@ -1,3 +1,20 @@
+'''
+Copyright (C) 2013 Travis DeWolf
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+'''
+
 from ..Arm import Arm
 import numpy as np
 import py1LinkArm
@@ -6,11 +23,7 @@ class Arm1Link(Arm):
     """A wrapper around a MapleSim generated C simulation 
     of a one link arm"""
 
-    def __init__(self, JEE='x', **kwargs):
-        """
-        JEE string: specifies whether the Jacobian for OSC control of 
-                    the end-effector should relay the x or y position.
-        """
+    def __init__(self, **kwargs):
 
         Arm.__init__(self, **kwargs)
 
