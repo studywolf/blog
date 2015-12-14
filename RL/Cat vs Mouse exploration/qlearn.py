cf873e5937da85c2b438b44de15d9f271f847dd0
travis dewolf <travis.dewolf@gmail.com>
2015-12-14 13:05:31 -0500

    removed outdated print functions

--- RL/Cat vs Mouse exploration/qlearn.py
+++ RL/Cat vs Mouse exploration/qlearn.py
@@ -41,29 +41,6 @@ class QLearn:
         maxqnew = max([self.getQ(state2, a) for a in self.actions])
         self.learnQ(state1, action1, reward, reward + self.gamma*maxqnew)
 
-    def printQ(self):
-        keys = self.q.keys()
-        states = list(set([a for a,b in keys]))
-        actions = list(set([b for a,b in keys]))
-        
-        dstates = ["".join([str(int(t)) for t in list(tup)]) for tup in states]
-        print (" "*4) + " ".join(["%8s" % ("("+s+")") for s in dstates])
-        for a in actions:
-            print ("%3d " % (a)) + \
-                " ".join(["%8.2f" % (self.getQ(s,a)) for s in states])
-
-    def printV(self):
-        keys = self.q.keys()
-        states = [a for a,b in keys]
-        statesX = list(set([x for x,y in states]))
-        statesY = list(set([y for x,y in states]))
-
-        print (" "*4) + " ".join(["%4d" % (s) for s in statesX])
-        for y in statesY:
-            maxQ = [max([self.getQ((x,y),a) for a in self.actions])
-                    for x in statesX]
-            print ("%3d " % (y)) + " ".join([ff(q,4) for q in maxQ])
-        
 import math
 def ff(f,n):
     fs = "{:f}".format(f)
@@ -71,12 +48,3 @@ def ff(f,n):
         return ("{:"+n+"s}").format(fs)
     else:
         return fs[:n]
-    # s = -1 if f < 0 else 1
-    # ss = "-" if s < 0 else ""
-    # b = math.floor(math.log10(s*f)) + 1
-    # if b >= n:
-    #     return ("{:" + n + "d}").format(math.round(f))
-    # elif b <= 0:
-    #     return (ss + ".{:" + (n-1) + "d}").format(math.round(f * 10**(n-1)))
-    # else:
-    #     return ("{:"+b+"d}.{:"+(n-b-1)+"
