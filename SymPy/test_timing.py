bccfe835793816602407eea320ed5c42f402a725
travis dewolf <travis.dewolf@gmail.com>
2016-08-28 14:42:34 -0400

    Sympy - added seaborn import to timing test

--- SymPy/test_timing.py
+++ SymPy/test_timing.py
@@ -15,6 +15,7 @@ You should have received a copy of the GNU General Public License
 along with this program.  If not, see <http://www.gnu.org/licenses/>.
 '''
 import timeit
+import seaborn
 
 # Test 1 ----------------------------------------------------------------------
 print('\nTest function 1: ')
